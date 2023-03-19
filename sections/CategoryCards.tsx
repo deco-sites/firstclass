45b3c8e4332f577a81a1660c1b45733650c5f0cf
ApMatheus <matheusaparecido101@gmail.com>
2023-03-19 10:54:21 -0300

    feat(gird category) grid from category

---
+++ sections/CategoryCards.tsx
@@ -0,0 +1,3 @@
+export { default } from "$store/components/ui/GridCategory.tsx";
+export type {Card, Props} from "$store/components/ui/GridCategory.tsx"
+export type {Image as LiveImage } from "deco-sites/std/components/types.ts";
